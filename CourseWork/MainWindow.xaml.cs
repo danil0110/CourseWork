e5a1b088ac7d1dd17e832783288d32df900507da
danil0110 <danilmail0110@gmail.com>
2020-05-07 20:41:00 +0300

    Added initialization of textBoxes for input

--- CourseWork/MainWindow.xaml.cs
+++ CourseWork/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace CourseWork
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            gd.Children.Clear();
+            int number = Convert.ToInt32(stepen.Text) + 1;
+            int[] odds = new int[number];
+            Label[] unknown = new Label[number];
+            TextBox[] oddBoxes = new TextBox[number];
+            int marginchik = 70, boxik = 43;
+            for (int i = 0; i < number - 1; i++)
+            {
+                oddBoxes[i] = new TextBox();
+                oddBoxes[i].Name = $"textBox{i + 1}";
+                oddBoxes[i].Margin = new Thickness(boxik, 73, 0, 0);
+                oddBoxes[i].Visibility = Visibility.Visible;
+                oddBoxes[i].Width = 27;
+                oddBoxes[i].HorizontalAlignment = HorizontalAlignment.Left;
+                oddBoxes[i].VerticalAlignment = VerticalAlignment.Top;
+                gd.Children.Add(oddBoxes[i]);
+                unknown[i] = new Label();
+                unknown[i].Name = $"label{i + 1}";
+                unknown[i].Margin = new Thickness(marginchik,70,0,0);
+                unknown[i].Visibility = Visibility.Visible;
+                if (number - i - 1 != 1)
+                    unknown[i].Content = $"x^{number - i - 1}";
+                else
+                    unknown[i].Content = "x";
+                gd.Children.Add(unknown[i]);
+                marginchik += 72;
+                boxik += 72;
+            }
+            oddBoxes[number - 1] = new TextBox();
+            oddBoxes[number - 1].Name = $"textBox{number}";
+            oddBoxes[number - 1].Margin = new Thickness(boxik, 73, 0, 0);
+            oddBoxes[number - 1].Visibility = Visibility.Visible;
+            oddBoxes[number - 1].Width = 27;
+            oddBoxes[number - 1].HorizontalAlignment = HorizontalAlignment.Left;
+            oddBoxes[number - 1].VerticalAlignment = VerticalAlignment.Top;
+            gd.Children.Add(oddBoxes[number - 1]);
+        }
     }
 }
