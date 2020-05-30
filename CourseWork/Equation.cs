f84047a8107d4717a851fcdcee1602b704914598
danil0110 <danilmail0110@gmail.com>
2020-05-30 15:37:37 +0300

    Remade Equation class

---
+++ CourseWork/Equation.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseWork
+{
+    public class Equation
+    {
+        public static string HalfDivide(double a, double b, int[] odds)
+        {
+            double f_a, f_b, f_c, x_c;
+
+            f_a = EquationFunction(odds, a);
+            f_b = EquationFunction(odds, b);
+
+            double sigma = 0.001;
+
+            while (Math.Abs(b - a) > sigma)
+            {
+                x_c = (a + b) / 2;
+                f_c = EquationFunction(odds, x_c);
+                if (f_a * f_c <= 0)
+                {
+                    b = x_c;
+                    f_b = f_c;
+                }
+                else
+                {
+                    a = x_c;
+                    f_a = f_c;
+                }
+
+            }
+
+            return $"x = {(a + b) / 2}\nf(x) = {EquationFunction(odds, (a + b) / 2)}";
+        }
+
+        public static string Newton(double x0, int[] odds)
+        {
+            double x1 = 0;
+            int[] f_der = Derivative(odds);
+            double sigma = 0.001;
+
+            while (Math.Abs(x1 - x0) > sigma)
+            {
+                x0 = x1;
+                x1 = x0 - EquationFunction(odds, x0) / EquationFunction(f_der, x0);
+            }
+            return $"x = {x1}\nf(x) = {EquationFunction(odds, x1)}";
+        }
+
+        public static string Secant(double x, double xLast, int[] odds)
+        {
+            double xGrandLast, sigma = 0.001;
+
+            while (Math.Abs(x - xLast) > sigma)
+            {
+                xGrandLast = xLast;
+                xLast = x;
+                x -= EquationFunction(odds, xLast) * (xLast - xGrandLast) / (EquationFunction(odds, xLast) - EquationFunction(odds, xGrandLast));
+            }
+            return $"x = {x}\nf(x) = {EquationFunction(odds, x)}";
+        }
+
+        public static string RootSegments(int[] odds, ref bool flag_answer)
+        {
+            string result = "";
+            double a = -10000;
+            while (a < 10000)
+            {
+                if (Equation.EquationFunction(odds, a) * Equation.EquationFunction(odds, a + 0.2) < 0)
+                {
+                    result += $"[{a}, {a + 0.2}]\n";
+                    flag_answer = true;
+                }
+
+                a += 0.2;
+            }
+
+            if (flag_answer == false)
+                return "Нет корней.";
+            else
+                return result;
+        }
+
+        public static double EquationFunction(int[] func, double num)
+        {
+            double result = 0;
+            for (int i = 0; i < func.Length; i++)
+            {
+                result += func[i] * Math.Pow(num, func.Length - i - 1);
+            }
+
+            return result;
+        }
+
+        private static int[] Derivative(int[] func)
+        {
+            int[] result = new int[func.Length - 1];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = func[i] * (func.Length - i - 1);
+            }
+
+            return result;
+        }
+    }
+}
--- CourseWork/MainWindow.xaml.cs
+++ CourseWork/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace CourseWork
         TextBox[] oddBoxes;
         int number, choice = 0;
         int[] odds;
+        bool flag_answer;
 
         public MainWindow()
         {
@@ -63,17 +64,6 @@ namespace CourseWork
             }
         }
 
-        private double EquationFunction(int[] func, double num)
-        {
-            double result = 0;
-            for (int i = 0; i < func.Length; i++)
-            {
-                result += func[i] * Math.Pow(num, func.Length - i - 1);
-            }
-
-            return result;
-        }
-
         private void radioButton_Checked(object sender, RoutedEventArgs e)
         {
             choice = 1; // метод половинного деления
@@ -98,35 +88,6 @@ namespace CourseWork
             input1_Copy.Text = "";
         }
 
-        private void rootSegments_Click(object sender, RoutedEventArgs e)
-        {
-            odds = new int[number];
-
-            for (int i = 0; i < number; i++)
-                odds[i] = Convert.ToInt32(oddBoxes[i].Text);
-
-            string result = "";
-            bool flag = false;
-            double a = -10000;
-            while (a < 10000)
-            {
-                if (EquationFunction(odds, a) * EquationFunction(odds, a + 0.2) < 0)
-                {
-                    result += $"[{a}, {a + 0.2}]\n";
-                    flag = true;
-                }
-
-                a += 0.2;
-            }
-
-            if (flag == false)
-            {
-                segments.Content = "Нет корней.";
-            }
-            else
-                segments.Content = result;
-        }
-
         private void radioButton_Copy1_Checked(object sender, RoutedEventArgs e)
         {
             choice = 3; // метод секущих
@@ -139,86 +100,34 @@ namespace CourseWork
             input1_Copy.Text = "";
         }
 
-        private void HalfDivide()
+        private void rootSegments_Click(object sender, RoutedEventArgs e)
         {
-            double a = Convert.ToDouble(input1.Text), b = Convert.ToDouble(input1_Copy.Text), f_a, f_b, f_c, x_c;
+            flag_answer = false;
             odds = new int[number];
-
             for (int i = 0; i < number; i++)
                 odds[i] = Convert.ToInt32(oddBoxes[i].Text);
 
-            f_a = EquationFunction(odds, a);
-            f_b = EquationFunction(odds, b);
-
-            double sigma = 0.001;
-
-            while (Math.Abs(b - a) > sigma)
-            {
-                x_c = (a + b) / 2;
-                f_c = EquationFunction(odds, x_c);
-                if (f_a * f_c <= 0)
-                {
-                    b = x_c;
-                    f_b = f_c;
-                }
-                else
-                {
-                    a = x_c;
-                    f_a = f_c;
-                }
-
-            }
-
-            answer.Content = $"x = {(a + b) / 2}\nf(x) = {EquationFunction(odds, (a + b) / 2)}";
-        }
-
-        private void Newton()
-        {
-            double x0 = Convert.ToDouble(input1.Text), x1 = 0;
-            int[] f_der = Derivative(odds);
-            double sigma = 0.001;
-
-            while (Math.Abs(x1 - x0) > sigma)
-            {
-                x0 = x1;
-                x1 = x0 - EquationFunction(odds, x0) / EquationFunction(f_der, x0);
-            }
-            answer.Content = $"x = {x1}\nf(x) = {EquationFunction(odds, x1)}";
-        }
-
-        private void Secant()
-        {
-            double x = Convert.ToDouble(input1.Text), xLast = Convert.ToDouble(input1_Copy.Text), xGrandLast, sigma = 0.001;
-
-            while (Math.Abs(x - xLast) > sigma)
-            {
-                xGrandLast = xLast;
-                xLast = x;
-                x -= EquationFunction(odds, xLast) * (xLast - xGrandLast) / (EquationFunction(odds, xLast) - EquationFunction(odds, xGrandLast));
-            }
-            answer.Content = $"x = {x}\nf(x) = {EquationFunction(odds, x)}";
+            segments.Content = Equation.RootSegments(odds, ref flag_answer);
         }
 
         private void Calculate_Click(object sender, RoutedEventArgs e)
         {
-            if (choice == 1)
-                HalfDivide();
-            else if (choice == 2)
-                Newton();
-            else if (choice == 3)
-                Secant();
-        }
-
-        private int[] Derivative(int[] func)
-        {
-            int[] result = new int[func.Length - 1];
-            for (int i = 0; i < result.Length; i++)
+            if (flag_answer)
             {
-                result[i] = func[i] * (func.Length - i - 1);
+                odds = new int[number];
+                for (int i = 0; i < number; i++)
+                    odds[i] = Convert.ToInt32(oddBoxes[i].Text);
+
+                if (choice == 1)
+                    answer.Content = Equation.HalfDivide(Convert.ToDouble(input1.Text), Convert.ToDouble(input1_Copy.Text), odds);
+                else if (choice == 2)
+                    answer.Content = Equation.Newton(Convert.ToDouble(input1.Text), odds);
+                else if (choice == 3)
+                    answer.Content = Equation.Secant(Convert.ToDouble(input1.Text), Convert.ToDouble(input1_Copy.Text), odds);
             }
-
-            return result;
+            else
+                answer.Content = "Нет корней.";
+            
         }
-
     }
 }
